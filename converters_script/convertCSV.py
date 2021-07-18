685f00d75a4c8be2ed95e7770a63367755c60956
matteoLorenzini <matteo-lorenzini@outlook.it>
2021-07-18 11:41:56 +0200

    Renamed folder

--- converters_script/convertCSV.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-import sys
-
-for f in sys.argv[1:]:
-	data = pd.read_csv(f, low_memory=False)
-	data.to_csv(f+'.tsv', sep='\t')
-
--- converters_script/csv2json.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-csv_data = pd.read_csv("input_data/vaw_dataset_coherence_cos.csv", sep = ";")
-print(csv_data)
-
-csv_data.to_json("vaw_dataset_coherence _SVM_.json", orient = "records")
